ff6b652e4410a189015378877013bde528382c9b
Johnny <jasiek.krol.pl@gmail.com>
2024-08-22 19:49:14 +0200

    api doc swagger

---
+++ src/OpenApiConfiguration.php
@@ -0,0 +1,17 @@
+<?php
+// src/OpenApiConfiguration.php
+
+namespace App;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="My API",
+ *     version="1.0.0",
+ *     description="API documentation for my Symfony project."
+ * )
+ */
+class OpenApiConfiguration
+{
+}
